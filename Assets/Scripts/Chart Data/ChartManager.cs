06ceccc528a363ff993650d1303b6064f3d4cc9d
cesardaher <daher.cesar@gmail.com>
2022-12-13 13:50:20 +0100

    Reorganized ChartManager
    Reorganized and annotated ChartManager

--- Assets/Scripts/Chart Data/ChartManager.cs
+++ Assets/Scripts/Chart Data/ChartManager.cs
@@ -97,37 +97,16 @@ public class ChartManager : MonoBehaviour
         // change data when game is playing
         // for changing the chart from inspector
         ReassignGeoData();
+
+        void ReassignGeoData()
+        {
+            if (_wasInitiated)
+                GeoData.ActiveData.InitializeData(_geodata.DataName, _day, _month, _year, _hour, _min, _sec, _geodata.D_timezone, _lat, _lon, _height, _hsys, _geodata.DaylightSavings);
+        }
     }
 
-    void ReCalculateChart()
-    {
-        CollectGeoData();
-        CalculateChart();
-    }
-
-    void CollectGeoData()
-    {
-        _Tjd_ut = _geodata.Tjd_ut;
-        _day = _geodata.Iday;
-        _month = _geodata.Imon;
-        _year = _geodata.Iyear;
-        _hour = _geodata.Ihour;
-        _min = _geodata.Imin;
-        _sec = _geodata.Dsec;
-        _timezone = _geodata.F_timezone;
-        _lat = _geodata.Geolat;
-        _lon = _geodata.Geolon;
-        _northernHemisphere = _geodata.NorthernHemisphere;
-        _height = _geodata.Height;
-        _hsys = _geodata.Hsys;
-    }
-
-    void ReassignGeoData()
-    {
-        if (_wasInitiated)
-            GeoData.ActiveData.InitializeData(_geodata.DataName, _day, _month, _year, _hour, _min, _sec, _geodata.D_timezone, _lat, _lon, _height, _hsys, _geodata.DaylightSavings);
-    }
-
+    // For initializing time
+    // Starts geodata based in Berlin's current time
     GeoData CreateCurrentTimeInBerlin()
     {
         int BerlinCityId = 12638;
@@ -151,19 +130,77 @@ public class ChartManager : MonoBehaviour
         return gd;
     }
 
+    // Recalculaltes Chart in runtime by updating GeoData and recalculating
+    void ReCalculateChart()
+    {
+        CollectGeoData();
+        CalculateChart();
+    }
+
+    // Collects data from selected GeoData
+    void CollectGeoData()
+    {
+        _Tjd_ut = _geodata.Tjd_ut;
+        _day = _geodata.Iday;
+        _month = _geodata.Imon;
+        _year = _geodata.Iyear;
+        _hour = _geodata.Ihour;
+        _min = _geodata.Imin;
+        _sec = _geodata.Dsec;
+        _timezone = _geodata.F_timezone;
+        _lat = _geodata.Geolat;
+        _lon = _geodata.Geolon;
+        _northernHemisphere = _geodata.NorthernHemisphere;
+        _height = _geodata.Height;
+        _hsys = _geodata.Hsys;
+    }
+
+    // Calculates chart based on GeoData
     void CalculateChart()
     {
+        // clears planetary conjunctions from previous calculation
         ConjunctionManager.Instance.ClearConjunctions();
 
-        // assign 2D chart elements
+        // assigns 2D chart elements
         DrawChart2D();
+
+        // calculates planetary positions
         CalculatePlanets();
+
+        // calculates and assigns Asc position
         AssignAsc();
+
+        // calculates and assigns MC position
         AssignMC();
+
+        // calls for finding planetary conjunctions
         ConjunctionManager.Instance.FindCloseConjunctions();
+
+        // positions planetary objects and symbols
         PlacePlanetSymbols();
+
         return;
 
+        // Draws 2D chart elements (zodiac, asc, mc, etc.)
+        // No planets
+        void DrawChart2D()
+        {
+            // assign chart positions 
+            _houseCusps = GeoData.ActiveData.HouseCusps;
+            _ascmc = GeoData.ActiveData.Ascmc;
+            _armc = GeoData.ActiveData.Armc;
+            _mc = GeoData.ActiveData.Mc;
+            _asc = GeoData.ActiveData.Asc;
+
+            // rotate whole zodiac
+            _zodiac.Rotation = -_asc;
+
+            // rotate meridian
+            // zodiac is meridian's parent
+            _meridian.Rotation = _mc;
+        }
+
+        // Calculates the planetary positions
         void CalculatePlanets()
         {
             // calculate planetary positions
@@ -202,26 +239,31 @@ public class ChartManager : MonoBehaviour
 
                 // assign positions to planets
                 AssignPlanets(p);
+            }
 
+
+            // Assigns planetary positions to relevant objects
+            void AssignPlanets(int planetId)
+            {
+                // for the given planet, assign ecliptic and horizontal positions
+                PlanetData planet = PlanetData.PlanetDataList[planetId];
+                double[] tempX2 = { _x2[0], _x2[1], _x2[2], _x2[3], _x2[4], _x2[5] };
+                double[] tempXaz = { _xaz[0], _xaz[1], _xaz[2], _xaz[3], _xaz[4], _xaz[5] };
+                double[] tempSecXaz = { _secAz[0], _secAz[1], _secAz[2], _secAz[3], _secAz[4], _secAz[5] };
+                planet.X2 = tempX2;
+                planet.Xaz = tempXaz;
+                planet.ChartAz = tempSecXaz;
+
+                // apply planetary info to relevant objects
+                EventManager.Instance.ApplyPlanetInfo(planetId);
+
+                // calculate south node mirroring north node
+                if (planet is NorthNodeData)
+                    CalculateSouthNode(planet);
             }
         }
-
-        void AssignPlanets(int planetId)
-        {
-            PlanetData planet = PlanetData.PlanetDataList[planetId];
-            double[] tempX2 = { _x2[0], _x2[1], _x2[2], _x2[3], _x2[4], _x2[5] };
-            double[] tempXaz = { _xaz[0], _xaz[1], _xaz[2], _xaz[3], _xaz[4], _xaz[5] };
-            double[] tempSecXaz = { _secAz[0], _secAz[1], _secAz[2], _secAz[3], _secAz[4], _secAz[5] };
-            planet.X2 = tempX2;
-            planet.Xaz = tempXaz;
-            planet.ChartAz = tempSecXaz;
-
-            EventManager.Instance.ApplyPlanetInfo(planetId);
-
-            if (planet is NorthNodeData)
-                CalculateSouthNode(planet);
-        }
-
+        
+        // Calculate positions for South Node as reflection of North Node's position
         void CalculateSouthNode(PlanetData node)
         {
             NorthNodeData northNode = node as NorthNodeData;
@@ -250,6 +292,7 @@ public class ChartManager : MonoBehaviour
             EventManager.Instance.ApplyPlanetInfo(southNode.planetID);
         }
 
+        // Calculates horizontal position of MC based on ecliptic position
         void AssignMC()
         {
             double[] mcX2 = { _mc, 0, 0, 0, 0, 0 };
@@ -262,6 +305,7 @@ public class ChartManager : MonoBehaviour
 
         }
 
+        // Calculates horizontal position of Asc based on ecliptic position
         void AssignAsc()
         {
             double[] ascX2 = { _asc, 0, 0, 0, 0, 0 };
@@ -272,16 +316,19 @@ public class ChartManager : MonoBehaviour
             _ascendant.X2 = ascX2;
             _ascendant.Xaz = az;
         }
-
     }
 
+    // Place planetary symbols in chart mode
     public void PlacePlanetSymbols()
     {
+        // get conjunction list from ConjunctionManager
         var conjunctionList = ConjunctionManager.Instance.ChartRegions.ToList();
 
+        // iterate through all planets,
+        // placing conjunct symbols together and remaining planets separately
         foreach(ChartRegion region in conjunctionList)
         {
-            // first deal with conjunctions
+            // first place conjunctions
             if(region is Conjunction)
             {
                 var conjunction = (Conjunction)region;
@@ -291,6 +338,8 @@ public class ChartManager : MonoBehaviour
                 {
                     PlanetData planet = PlanetData.PlanetDataList[activePlanetIDs[j]];
 
+                    // if a conjunction has only one active planet
+                    // place symbol like a regular planet
                     if (activePlanetIDs.Count == 1)
                     {
                         planet.PlaceSymbols();
@@ -306,29 +355,12 @@ public class ChartManager : MonoBehaviour
             }
 
 
-            // deal with single planets
+            // place single planets
             var inconjunctPlanet = (InconjunctPlanet)region;
             inconjunctPlanet.Planet.PlaceSymbols();
             inconjunctPlanet.Planet.PlaceSymbols2D();
         }
 
-    }
-
-    void DrawChart2D()
-    {
-        _houseCusps = GeoData.ActiveData.HouseCusps;
-        _ascmc = GeoData.ActiveData.Ascmc;
-
-        _armc = GeoData.ActiveData.Armc;
-        _mc = GeoData.ActiveData.Mc;
-        _asc = GeoData.ActiveData.Asc;
-
-        // rotate whole zodiac
-        _zodiac.Rotation = -_asc;
-
-        // rotate meridian
-        // zodiac is meridian's parent
-        _meridian.Rotation = _mc;
     }
 
 }
